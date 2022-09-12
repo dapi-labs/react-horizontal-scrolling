87b257201ae012d195320201f10720a8e9ef7e49
wwayne <wayne.wang0821@gmail.com>
2022-09-12 12:37:20 +0800

    v0.1.10: replace node-sass with sass and add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export interface HorizontalScrollProps {
+  className?: string,
+  disabled?: boolean
+}
+
+export default class HorizontalScroll extends React.Component<HorizontalScrollProps> {}
